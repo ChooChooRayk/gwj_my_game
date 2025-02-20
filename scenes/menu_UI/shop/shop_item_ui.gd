80bd8a2eedae49e35ee9819d8382ea9016daeccd
ChooChooRayk <choochoorayk@gmail.com>
2025-02-20 10:45:16 +0100

    double click buy in shop

--- scenes/menu_UI/shop/shop_item_ui.gd
+++ scenes/menu_UI/shop/shop_item_ui.gd
@@ -2,6 +2,7 @@ class_name ShopItemUI
 extends Button
 
 var item_res : TemperingTool
+var shop_ui  : ShopUI
 
 # ====== INITIALIZATION ====== #
 
@@ -9,6 +10,19 @@ func _ready() -> void:
     if is_instance_valid(item_res):
         icon = item_res.icon
         text = "cost : {price}$".format({"price":item_res.price})
+    # ---
+    shop_ui = Utilities.find_first_parent_of_type(self, ShopUI) as ShopUI
     return
 
+# ====== PROCESS ====== #
+
+func _gui_input(event: InputEvent) -> void:
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.double_click:
+        auto_buy_item()
+        
 # ====== MANAGEMENT ====== #
+
+func auto_buy_item()->void:
+    if is_instance_valid(shop_ui):
+        shop_ui.on_buying()
+    return
