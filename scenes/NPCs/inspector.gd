641ae734cc11e5e38e7e6f806f156c26aa9c3c62
ChooChooRayk <choochoorayk@gmail.com>
2025-02-21 10:42:39 +0100

    change catching mechanic

--- scenes/NPCs/inspector.gd
+++ scenes/NPCs/inspector.gd
@@ -3,3 +3,5 @@ extends BodyMotor
 
 @export var scouting_targets: Path2D
 @export var scouting_speed  := 50.
+
+@onready var catching_zone: Area2D = %CatchingZone
--- scenes/NPCs/inspector_ai/InspectorChasingState.gd
+++ scenes/NPCs/inspector_ai/InspectorChasingState.gd
@@ -7,6 +7,8 @@ extends InspectorState
 func _ready() -> void:
     super()
     set_process(false)
+    if is_instance_valid((inspector_ai.npc_body as Inspector).catching_zone):
+        (inspector_ai.npc_body as Inspector).catching_zone.body_entered.connect(on_catching_body)
 
 # ====== PROCESS ====== #
 
@@ -18,13 +20,21 @@ func _process(_delta: float) -> void:
 func enter()->void:
     set_process(true)
     inspector_ai.go_to_target = true
+    (inspector_ai.npc_body as Inspector).catching_zone.monitoring = true
     return
     
 func exit()->void:
     set_process(false)
+    (inspector_ai.npc_body as Inspector).catching_zone.monitoring = false
     return
 
-func target_reached()->void:
-    EventBus.SuspectCaught.emit(inspector_ai.target_to_chase)
-    ChangeStateRequested.emit(self, STATES.Idle)
+#func target_reached()->void:
+    #EventBus.SuspectCaught.emit(inspector_ai.target_to_chase)
+    #ChangeStateRequested.emit(self, STATES.Idle)
+    #return
+    
+func on_catching_body(body:Node2D)->void:
+    if body.is_in_group("player"):
+        EventBus.SuspectCaught.emit(inspector_ai.target_to_chase)
+        ChangeStateRequested.emit(self, STATES.Idle)        
     return
