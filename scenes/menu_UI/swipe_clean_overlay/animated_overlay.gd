888c0ee61e2874f032a51616c64995b159a53238
ChooChooRayk <choochoorayk@gmail.com>
2025-02-21 23:58:39 +0100

    main menu button animation

---
+++ scenes/menu_UI/swipe_clean_overlay/animated_overlay.gd
@@ -0,0 +1,34 @@
+class_name AnimatedOverlay
+extends MarginContainer
+
+
+@onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
+
+var control_parent : Control
+
+# ====== INITIALIZATION ====== #
+
+func _ready() -> void:
+    control_parent = get_parent() as Control
+    # ---
+    control_parent.mouse_entered.connect(play_animation)
+    control_parent.mouse_exited .connect(set_to_default)
+    control_parent.resized      .connect(set_aspect_to_center)
+    return
+
+# ====== PROCESS ====== #
+
+func set_aspect_to_center()->void:
+    #var frame_size := animated_sprite_2d.sprite_frames.get_frame_texture("default",0).get_size()
+    animated_sprite_2d.position = control_parent.size/2.# - frame_size/2.
+    return
+
+# ====== MANAGEMENT ====== #
+
+func play_animation()->void:
+    animated_sprite_2d.play("swipe_clean")
+    return
+
+func set_to_default()->void:
+    animated_sprite_2d.play("default")
+    return
