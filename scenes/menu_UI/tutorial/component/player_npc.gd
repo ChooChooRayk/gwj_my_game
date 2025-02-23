4ff46707f8a2eab79b02ebdb517d95cb3c387cdb
ChooChooRayk <choochoorayk@gmail.com>
2025-02-23 02:25:37 +0100

    tuto panel 1

--- scenes/menu_UI/tutorial/component/player_npc.gd
+++ scenes/menu_UI/tutorial/component/player_npc.gd
@@ -2,6 +2,9 @@ class_name PlayerNPC
 extends BodyMotor
 
 
+@export var enable_cleaning_zone := false
+@export var enable_framing_zone  := false
+@export_category("Movement")
 @export var walking_speed : float = 100.
 @export var running_speed : float = 100.
 
@@ -20,19 +23,11 @@ func _ready() -> void:
     if is_instance_valid(framing_zone):
         framing_zone .clickable_zone = PlayerStatistics.current_framing_tool .clickable_zone
     # ---
-    EventBus.EnableCleaningZoneDisplay.connect(
-        func():
-            cleaning_zone.enable_zone_aspect(true)
-            framing_zone .enable_zone_aspect(false)
-    )
-    EventBus.EnableFramingZoneDisplay .connect(
-        func():
-            cleaning_zone.enable_zone_aspect(false)
-            framing_zone .enable_zone_aspect(true)
-    )
+    cleaning_zone.enable_zone_aspect(enable_cleaning_zone)
+    framing_zone .enable_zone_aspect(enable_framing_zone)
     # ---
-    PlayerStatistics.CurrentToolUpdated.connect(set_draw_zone_res)
-    EventBus.EnableCleaningZoneDisplay.emit()
+    #PlayerStatistics.CurrentToolUpdated.connect(set_draw_zone_res)
+    #EventBus.EnableCleaningZoneDisplay.emit()
     # ---
     set_physics_process(false)
 
