80fb0356f72ba295e337820bea39c93a41be7572
ChooChooRayk <choochoorayk@gmail.com>
2025-02-23 20:35:03 +0100

    level 2

--- global_scripts/MissionConversations.gd
+++ global_scripts/MissionConversations.gd
@@ -10,6 +10,9 @@ enum CONV {
     CONV_2_LEVEL,
     CONV_2_END,
     # ---
+    CONV_3_START,
+    CONV_3_LEVEL,
+    CONV_3_END,
 }
 
 static var conversation_dic := {
@@ -34,4 +37,57 @@ static var conversation_dic := {
         ["Agent" , ["..."]],
         ["Mme M", ["I am again ery grateful... Although I might need your service again soon...this anoying neighbouring want to be singer..."]],
     ],
+    # ------ #
+    CONV.CONV_2_START:[
+        ["D0nC0rn3rt!o" , ["Good evening, sir. I require your service as soon as possible."]],
+        ["Agent" , ["I'm listening."]],
+        ["D0nC0rn3rt!o", ["I sent my dear nephew to resolve a business matter, if you follow me. However my nephew is a messy little fella. See what I mean ?"]],
+        ["Agent" , ["I think I do."]],
+        ["D0nC0rn3rt!o" , ["Good, good.", "Then I can count on you. I'm very generous with people that satisfies me... and with the poeple that doesn't, well I send my nephew."]],
+        ["Agent" , ["Don't worry it wiil be done."]],
+        ["D0nC0rn3rt!o" , ["By the, theres' this investigator that seems quite motivated to solve all those ... unsolved accidents. I would suggest you to work quickly."]],
+    ],
+    CONV.CONV_2_LEVEL:[
+        ["Inspector 1", ["Geez, this what a mess this one is. No doubt who did that but where are the traces?"]],
+        ["Agent" , ["Hi, just passing by. I brought you coffee."]],
+        ["Inspector 2", ["Thanks I needed that !"]],
+        ["Agent" , ["..."]],
+        ["Inspector 3" , ["First time on the job, and I got this case. No clue what's happened here."]],
+        ["Agent" , ["Not really my place, but probably an accident."]],
+        ["Inspector 3" , ["I really doubt it..."]],
+        ["Agent" , ["..."]],
+    ],
+    CONV.CONV_2_END:[
+        ["D0nC0rn3rt!o", ["Well done, agent, I never doubted you. I'll send you the rest of your reward diligently."]],
+        ["Agent" , ["No worry."]],
+        ["Mme M", ["I'll make sure to save you place in my contacts."]],
+        ["Agent" , ["...thank you sir."]],
+    ],
+    # ------ #
+    CONV.CONV_3_START:[
+        ["D0nC0rn3rt!o" , ["Good evening, sir. I require your service as soon as possible."]],
+        ["Agent" , ["I'm listening."]],
+        ["D0nC0rn3rt!o", ["I sent my dear nephew to resolve a business matter, if you follow me. However my nephew is a messy little fella. See what I mean ?"]],
+        ["Agent" , ["I think I do."]],
+        ["D0nC0rn3rt!o" , ["Good, good.", "Then I can count on you. I'm very generous with people that satisfies me... and with the poeple that doesn't, well I send my nephew."]],
+        ["Agent" , ["Don't worry it wiil be done."]],
+        ["D0nC0rn3rt!o" , ["By the, theres' this investigator that seems quite motivated to solve all those ... unsolved accidents. I would suggest you to work quickly."]],
+    ],
+    CONV.CONV_3_LEVEL:[
+        ["Inspector 1", ["Geez, this what a mess this one is. No doubt who did that but where are the traces?"]],
+        ["Agent" , ["Hi, just passing by. I brought you coffee."]],
+        ["Inspector 2", ["Thanks I needed that !"]],
+        ["Agent" , ["..."]],
+        ["Inspector 3" , ["First time on the job, and I got this case. No clue what's happened here."]],
+        ["Agent" , ["Not really my place, but probably an accident."]],
+        ["Inspector 3" , ["I really doubt it..."]],
+        ["Agent" , ["..."]],
+    ],
+    CONV.CONV_3_END:[
+        ["D0nC0rn3rt!o", ["Well done, agent, I never doubted you. I'll send you the rest of your reward diligently."]],
+        ["Agent" , ["No worry."]],
+        ["Mme M", ["I'll make sure to save you place in my contacts."]],
+        ["Agent" , ["...thank you sir."]],
+    ],
+
 }
