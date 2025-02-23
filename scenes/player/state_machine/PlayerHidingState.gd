63724d73b1b17f814ee65e990a1b73c80409d431
ChooChooRayk <choochoorayk@gmail.com>
2025-02-23 13:29:41 +0100

    state machine update

--- scenes/player/state_machine/PlayerHidingState.gd
+++ scenes/player/state_machine/PlayerHidingState.gd
@@ -1,2 +1,22 @@
 class_name PlayerHidingState
 extends PlayerState
+
+@export var hiding_sprite_frame : SpriteFrames
+
+# ====== INITIALIZATION ====== #
+
+func _ready() -> void:
+    super()
+
+# ====== MANAGEMENT ====== #
+
+func enter()->void:
+    if not(is_instance_valid(hiding_sprite_frame)):
+        push_error("not hiding frame set !")
+        return
+    # ---
+    input_player.player_body.animated_sprite_2d.sprite_frames = hiding_sprite_frame
+    return
+    
+func exit()->void:
+    return
--- scenes/player/state_machine/PlayerNormalState.gd
+++ scenes/player/state_machine/PlayerNormalState.gd
@@ -1,2 +1,22 @@
 class_name PlayerNormalState
 extends PlayerState
+
+
+@export var normal_sprite_frame : SpriteFrames
+
+# ====== INITIALIZATION ====== #
+
+func _ready() -> void:
+    super()
+    # ---
+    if not(is_instance_valid(normal_sprite_frame)):
+        normal_sprite_frame = input_player.player_body.animated_sprite_2d.sprite_frames
+
+# ====== MANAGEMENT ====== #
+
+func enter()->void:
+    input_player.player_body.animated_sprite_2d.sprite_frames = normal_sprite_frame
+    return
+    
+func exit()->void:
+    return
