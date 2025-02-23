e78d251625f68226c618aa19a3ebba14fc4648a6
ChooChooRayk <choochoorayk@gmail.com>
2025-02-23 11:39:16 +0100

    content warning and splash screen fixes

--- scenes/start_spalsh_screens/splash_screens.gd
+++ scenes/start_spalsh_screens/splash_screens.gd
@@ -8,8 +8,9 @@ signal SplashScreensFinished()
 @export var trans_duration   := 0.5 # [s]
 @export var display_duration := 2.0 # [s]
 
-@onready var splash_screen_intro_0: Control = $SplashScreenIntro0
-@onready var splash_screen_intro_1: Control = $SplashScreenIntro1
+@onready var splash_screen_intro_0  : Control = $SplashScreenIntro0
+@onready var splash_screen_intro_1  : Control = $SplashScreenIntro1
+@onready var splash_content_warning : Control = $SplashContentWarning
 
 @onready var trans_overlay: SceneTransitionOverlay = $SceneTransitionOverlay
 
@@ -26,6 +27,7 @@ func _ready() -> void:
     # ---
     splash_scrn_list.append(splash_screen_intro_0)
     splash_scrn_list.append(splash_screen_intro_1)
+    splash_scrn_list.append(splash_content_warning)
     # ---
     for child in splash_scrn_list:
         child.visible = false
@@ -45,7 +47,7 @@ func next_scren()->void:
     if trans_overlay.animation_player.is_playing():# tackle the asynchronicity
         await trans_overlay.animation_player.animation_finished
     # ---
-    #print("splash screen count : ", screen_count)
+    print("splash screen count : ", screen_count)
     if screen_count>=splash_scrn_list.size():
         trans_overlay.transition(SceneTransitionOverlay.TRANS_TYPE.FADE_IN)
         await trans_overlay.transition_finished
@@ -61,7 +63,7 @@ func next_scren()->void:
     splash_scrn_list[screen_count].visible = false
     # ---
     screen_count += 1
-    next_scren()
+    await next_scren()
 
 func interupt_splash()->void:
     trans_overlay.skip_transition()
@@ -69,7 +71,7 @@ func interupt_splash()->void:
     if screen_count>=splash_scrn_list.size():
         end_splash_screens()
         return
-    next_scren()
+    await next_scren()
 
 func end_splash_screens()->void:
         SplashScreensFinished.emit()
